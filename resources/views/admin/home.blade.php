ace74012d2838af9096608da55b4fc02ab322295
Cilyan Olowen <gaknar@gmail.com>
2019-04-02 21:58:39 +0200

    Just some polish to admin

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -51,7 +51,7 @@
         </div>
     </div>
     <div class="row">
-        <div class="col-md-6">
+        <div class="col-lg-6 mb-4">
             <div class="card shadow">
                 <div class="card-header">
                     <h6 class="my-1 font-weight-bold">
@@ -88,6 +88,21 @@
                             <i class="fas fa-plus"></i> {{  __('Add one') }}
                         </a>
                     </div>
+                </div>
+            </div>
+        </div>
+        <div class="d-none d-lg-block col-lg-6 mb-4">
+            <!-- Illustrations -->
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold">
+                      {{ config('app.name', 'Laravel') }}
+                  </h6>
+                </div>
+                <div class="card-body">
+                  <div class="text-center">
+                    <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;" src="{{ asset('svg/undraw_finish_line_katerina_limpitsouni_xy20.svg') }}" alt="">
+                  </div>
                 </div>
             </div>
         </div>
