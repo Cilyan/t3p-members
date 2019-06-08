ca369d49309b5bf0493a16682f951725425341a2
Cilyan Olowen <gaknar@gmail.com>
2019-06-09 00:07:12 +0200

    Update JS dependencies
    And prepare for sidebar

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,5 +11,21 @@ window.$ = window.jQuery = require('jquery')
 require('bootstrap');
 
 $(function () {
-  $('[data-toggle="tooltip"]').tooltip()
+    // Tooltip handling
+    $('[data-toggle="tooltip"]').tooltip()
+
+    // Toggle the side navigation
+    function sidebar_toggler(e) {
+        $("body").toggleClass("sidebar-toggled")
+        $(".sidebar").toggleClass("toggled")
+        if ($(".sidebar").hasClass("toggled")) {
+            $('.sidebar .collapse').collapse('hide')
+        }
+    }
+
+    $("#sidebarToggle, #sidebarToggleTop").on('click', sidebar_toggler)
+
+    if ($( window ).width() < 992) {
+        sidebar_toggler(null)
+    }
 })
