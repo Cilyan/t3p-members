f2273d7ed901eba3aaecff2ffa00694eb291fb34
Cilyan Olowen <gaknar@gmail.com>
2020-03-07 16:39:55 +0100

    Forgotten issue with migration to Laravel 6.X
    https://laravel.com/docs/6.x/upgrade#email-verification-route

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -6,13 +6,16 @@
 
     <div class="text-center">
         <h1 class="h4 text-gray-900 mb-4">@lang('Verify Your Email Address')</h1>
-        <p class="mb-4">
-            @lang('Before proceeding, please check your email for a verification link.')
-            @lang('If you did not receive the email'),
-            <a href="{{ route('verification.resend') }}">
-                @lang('click here to request another')
-            </a>.
-        </p>
+        <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+            @csrf
+            <p class="mb-4">
+                @lang('Before proceeding, please check your email for a verification link.')
+                @lang('If you did not receive the email'),
+                <button type="submit" class="btn btn-link p-0 m-0 align-baseline">
+                    @lang('click here to request another')
+                </button>.
+            </p>
+        </form>
         <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 15rem;" src="{{ asset('svg/undraw_mail_cg1t.svg') }}" alt="">
     </div>
     <hr>
