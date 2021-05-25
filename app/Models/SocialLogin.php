226c7f897fe8f2963788f8df6f57fa3ccbe31076
Cilyan Olowen <gaknar@gmail.com>
2021-05-25 09:50:28 +0200

    Bugfix belongsTo relationship in SocialLogin
    - Missing change in SocialLogin when models were moved
      to Models sub-directory
    
    Signed-off-by: Cilyan Olowen <gaknar@gmail.com>

--- app/Models/SocialLogin.php
+++ app/Models/SocialLogin.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 
 class SocialLogin extends Model
@@ -26,6 +27,6 @@ class SocialLogin extends Model
 
     public function user()
     {
-        return $this->belongsTo('App\User');
+        return $this->belongsTo(User::class);
     }
 }
